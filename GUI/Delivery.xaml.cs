872ec0519463d5399f4b01143d7c7866c6ca672c
Karol Podlewski <karol.podlewski@hotmail.com>
2018-12-17 00:08:20 +0100

    Fixed errors

--- GUI/Delivery.xaml.cs
+++ GUI/Delivery.xaml.cs
@@ -23,5 +23,40 @@ namespace GUI
         {
             InitializeComponent();
         }
+
+        private void Delivery1RB_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delivery2RB_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Address1RB_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Address2RB_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackMenuB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackCartB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToPaymentB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
