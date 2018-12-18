28d762f489d6581aae47aab45e85f3586575e1c9
Daro1Daro <dariusz.malagowski@gmail.com>
2018-12-18 01:09:52 +0100

    Add files via upload

---
+++ Admin/Address.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    public class Address
+    {
+        public int _id;
+        public string _street;
+        public string _postalCode;
+        public string _city;
+        public string _number;
+
+        public Address(int id, string street, string number, string postalCode, string city)
+        {
+            _id = id;
+            _street = street;
+            _number = number;
+            _postalCode = postalCode;
+            _city = city;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Street
+        {
+            get { return _street; }
+            set { _street = value; }
+        }
+        public string PostalCode
+        {
+            get { return _postalCode; }
+            set { _postalCode = value; }
+        }
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+        public string Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+    }
+}
---
+++ Admin/OrderManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    public class OrderManager
+    {
+        private void getProducts()
+        {
+
+        }
+    }
+}
