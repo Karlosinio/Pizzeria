d2acd794409ee89dc943bf38a3893a09e60c457d
Bartłomiej Ozurkiewicz <b.ozurkiewicz@gmail.com>
2019-01-13 19:18:53 +0100

    ++

---
+++ CartBackend/Common/DTO/ComponentDTO.cs
@@ -0,0 +1,15 @@
+﻿using CartBackend.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartBackend.Common.DTO
+{
+    public class ComponentDTO
+    {
+        public int Quantity { get; set; }
+        public Component Component { get; set; }
+    }
+}
