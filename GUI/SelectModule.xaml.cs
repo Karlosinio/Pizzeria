ddcb3957c0ff6eca79e39dc8974250b6e30c0357
Karol Podlewski <karol.podlewski@hotmail.com>
2019-01-18 22:25:30 +0100

    Select Module Added

---
+++ GUI/SelectModule.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace GUI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy RestorePassword.xaml
+    /// </summary>
+    public partial class SelectModule : Window
+    {
+        public SelectModule()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Menu(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new Menu();
+            Close();
+            newWindow.Show();
+        }
+
+        private void Button_Admin(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new Orders();
+            Close();
+            newWindow.Show();
+        }
+
+        private void Button_Logout(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new LoggingPanel();
+            Close();
+            newWindow.Show();
+        }
+    }
+}
